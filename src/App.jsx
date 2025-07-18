91f3daf38eee62d193bd3c147b0a0ea6451195d8
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-07-18 14:09:35 +0530

    add appStore

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
 import Body from "./Components/Body";
+import { Provider } from "react-redux";
+
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
